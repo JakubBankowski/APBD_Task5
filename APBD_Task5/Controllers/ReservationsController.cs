6ca85b9dbe4d8868e38fbaf7d0e5a0cf0108f67e
polishdev_ <baniakubabania@wp.pl>
2026-04-21 11:14:49 +0200

    Finished task

--- APBD_Task5/Controllers/ReservationsController.cs
+++ APBD_Task5/Controllers/ReservationsController.cs
@@ -8,9 +8,28 @@ namespace APBD_Task5.Controllers;
 public class ReservationsController : ControllerBase
 {
     [HttpGet]
-    public ActionResult<List<Reservation>> GetAll()
+    public ActionResult<List<Reservation>> GetAll(
+        [FromQuery]DateOnly? date,
+        [FromQuery]string? status,
+        [FromQuery]int? roomId
+        )
     {
         var reservations = DataStore.Reservations.AsEnumerable();
+
+        if (date.HasValue)
+        {
+            reservations = reservations.Where(r => r.Date == date);
+        }
+
+        if (!string.IsNullOrWhiteSpace(status))
+        {
+            reservations = reservations.Where(r => r.Status.Equals(status, StringComparison.OrdinalIgnoreCase));
+        }
+
+        if (roomId.HasValue)
+        {
+            reservations = reservations.Where(r => r.RoomId == roomId);
+        }
         return Ok(reservations.ToList());
     }
     
@@ -23,5 +42,52 @@ public class ReservationsController : ControllerBase
             return NotFound($"Reservation with id {id} not found.");
         }
         return Ok(reservation);
+    }
+
+    [HttpPost]
+    public ActionResult<Reservation> CreateReservation(Reservation reservation)
+    {
+        var roomExists = DataStore.Rooms.Any(r => r.Id == reservation.Id);
+        if (!roomExists) return BadRequest($"Room with id {reservation.RoomId} does not exist.");
+        reservation.Id = DataStore.NextReservationId;
+        DataStore.Reservations.Add(reservation);
+        
+        return CreatedAtAction(nameof(GetById), new { id = reservation.Id }, reservation);
+    }
+    
+    [HttpPut("{id:int}")]
+    public ActionResult UpdateReservation(int id, [FromBody] Reservation reservation)
+    {
+        var index = DataStore.Reservations.FindIndex(r => r.Id == id);
+
+        if (index == -1)
+        {
+            return NotFound($"Reservation with id {id} not found.");
+        }
+
+        var roomExists = DataStore.Rooms.Any(r => r.Id == reservation.RoomId);
+        if (!roomExists)
+        {
+            return BadRequest($"Room with id {reservation.RoomId} does not exist.");
+        }
+
+        reservation.Id = id;
+        DataStore.Reservations[index] = reservation;
+
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public ActionResult DeleteReservation(int id)
+    {
+        var reservation = DataStore.Reservations.FirstOrDefault(r => r.Id == id);
+
+        if (reservation == null)
+        {
+            return NotFound($"Reservation with id {id} not found.");
+        }
+
+        DataStore.Reservations.Remove(reservation);
+        return NoContent();
     }
 }
