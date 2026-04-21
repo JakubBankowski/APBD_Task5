adc69b3166e97495b80a6b8773a7aeaa9faee779
JakubBankowski <s33203@pjwstk.edu.pl>
2026-04-21 08:05:39 +0200

    Initial commit

---
+++ APBD_Task5/Controllers/RoomsController.cs
@@ -0,0 +1,28 @@
+﻿
+using APBD_Task5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_Task5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Room>> GetAll()
+    {
+        var rooms = DataStore.Rooms.AsEnumerable();
+        return Ok(rooms.ToList());
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Room> GetById(int id)
+    {
+        var room = DataStore.Rooms.FirstOrDefault(r => r.Id == id);
+        if (room == null)
+        {
+            return NotFound($"Room with id {id} not found.");
+        }
+        return Ok(room);
+    }
+}
---
+++ APBD_Task5/Models/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD_Task5.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; } = string.Empty;
+    
+    [Required]
+    [MinLength(1)]
+    public string BuildingCode { get; set; } = string.Empty;
+    public int Floor { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Capacity must be greater than or equal to 1")]
+    public int Capacity { get; set; }
+    public bool HasProjector{get; set;}
+    public bool IsActive { get; set; }
+}
